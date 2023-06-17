279bc7ab3db5541c2bcd6d607630da71b02c5457
Alan Estrada <joshualan23@gmail.com>
2023-06-17 18:56:45 -0400

    create repository for front-end

---
+++ intus-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Whereas recognition of the inherent dignity</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
